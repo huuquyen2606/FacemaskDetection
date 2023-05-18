aa750b1c60f19f84f309b8f08c16c726f342729a
huuquyen2606 <huuquyen2606@gmail.com>
2023-05-18 14:34:02 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,72 @@
+from keras.optimizers import RMSprop
+import cv2
+from keras.models import Sequential
+from keras.layers import Conv2D, Input, ZeroPadding2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense,Dropout
+from keras.models import Model, load_model
+import numpy as np
+
+model =Sequential([
+    Conv2D(100, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(2,2),
+    
+    Conv2D(100, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    
+    Flatten(),
+    Dropout(0.5),
+    Dense(50, activation='relu'),
+    Dense(2, activation='softmax')
+])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model.load_weights("myModel.h5")
+
+labels_dict={0:'without mask',1:'mask'}
+color_dict={0:(0,0,255),1:(0,255,0)}
+
+size = 4
+webcam = cv2.VideoCapture(0) #Use camera 0
+
+# We load the xml file
+# classifier = cv2.CascadeClassifier('/home/shivam/.local/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+classifier = cv2.CascadeClassifier("C:/Users/huuva/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+
+
+while True:
+    (rval, im) = webcam.read()
+    im=cv2.flip(im,1,1) #Flip to act as a mirror
+
+    # Resize the image to speed up detection
+    mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
+
+    # detect MultiScale / faces 
+    faces = classifier.detectMultiScale(mini)
+
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
+        #Save just the rectangle faces in SubRecFaces
+        face_img = mini #im[y:y+h, x:x+w]
+        resized=cv2.resize(face_img,(150,150))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,150,150,3))
+        reshaped = np.vstack([reshaped])
+        result = model.predict(reshaped)
+        print(result)
+        
+        label=np.argmax(result,axis=1)[0]
+      
+        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+    # Show the image
+    cv2.imshow('LIVE',   im)
+    key = cv2.waitKey(10)
+    # if Esc key is press then break out of the loop 
+    if key == 27: #The Esc key
+        break
+# Stop video
+webcam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
